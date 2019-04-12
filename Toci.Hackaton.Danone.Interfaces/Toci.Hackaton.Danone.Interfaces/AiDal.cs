70e469921b8b82c18281115d114a4f482e069b04
Bartlomiej Zapart <Bartlomiej.Zapart@cdsazure.onmicrosoft.com>
2019-04-13 00:55:18 +0200

    dal

--- Toci.Hackaton.Danone.Interfaces/Toci.Hackaton.Danone.Interfaces/AiDal.cs
+++ Toci.Hackaton.Danone.Interfaces/Toci.Hackaton.Danone.Interfaces/AiDal.cs
@@ -2,10 +2,34 @@
 //   Copyright (c) 2019 Rolls-Royce plc
 // </copyright>
 
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime;
+
 namespace Toci.Hackaton.Danone.Interfaces
 {
     public class AiDal
     {
+        public List<string> Tables = new List<string>()
+        {
+            "recipe_0_processing_details_slurry",
+            "recipe_0_processing_details_dd"
+        };
+
+        public Dictionary<string, object> Results = new Dictionary<string, object>();
+        public virtual Dictionary<string, object> GetAggregations()
+        {
+            hackathon_danoneEntities ent = new hackathon_danoneEntities();
+
+            foreach (string table in Tables)
+            {
+                Results.Add(table, ent.recipe_0_orders_details.Include(table));
+            }
+
+            return Results;
+        }
+
         
     }
 }
---
+++ Toci.Hackaton.Danone.Interfaces/Toci.Hackaton.Danone.Interfaces/Entity.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="Entity.cs" company="Rolls-Royce plc">
+//   Copyright (c) 2019 Rolls-Royce plc
+// </copyright>
+
+namespace Toci.Hackaton.Danone.Interfaces
+{
+    public class Entity
+    {
+        
+    }
+}
