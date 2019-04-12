dcba86761a27243adfb6a0a2963e412d3b5abf8c
Bartlomiej Zapart <Bartlomiej.Zapart@cdsazure.onmicrosoft.com>
2019-04-13 00:56:29 +0200

    bll

---
+++ Toci.Hackaton.Danone.Interfaces/Toci.Hackaton.Danone.Interfaces/AggregationBll.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="AggregationBll.cs" company="Rolls-Royce plc">
+//   Copyright (c) 2019 Rolls-Royce plc
+// </copyright>
+
+namespace Toci.Hackaton.Danone.Interfaces
+{
+    public class AggregationBll
+    {
+        
+    }
+}
